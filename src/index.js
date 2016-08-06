9ca106a66e45f86532b0de5620aeac6865ee0382
Dave Coates <david.coates@alliancesoftware.com.au>
2016-08-06 15:16:51 +1000

    Ready for publish

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import getEntries from './getEntries';
+import getProperties from './getProperties';
+import buildMirror from './mirror';
+import serialize, { registerObjectSerializer } from './serialize';
+
+export {
+    getEntries,
+    getProperties,
+    buildMirror,
+    serialize,
+    registerObjectSerializer,
+};
