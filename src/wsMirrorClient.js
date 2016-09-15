34003d878cbb00b5d5b5bd0782a1d749a3d52fd3
Dave Coates <david.coates@alliancesoftware.com.au>
2016-09-15 18:06:04 +1000

    Add event handler fn's for ws client

--- src/wsMirrorClient.js
+++ src/wsMirrorClient.js
@@ -66,34 +66,72 @@ type RejectResolve = {
     resolve: (object:{}) => void;
 };
 
+type UnsubscribeFunction = () => void;
+type CloseEventCallback = (closeEvent:CloseEvent) => void;
+type MessageEventCallback = (messageEvent:MessageEvent) => void;
+type NoParamCallback = () => void;
+
 export default class WebSocketMirrorClient {
 
     id: string;
     ws: WebSocket;
     waitingResponse:Map<string, RejectResolve> = new Map();
+    onMessageHandlers:Array<MessageEventCallback> = [];
+    onOpenHandlers:Array<NoParamCallback> = [];
+    onErrorHandlers:Array<NoParamCallback> = [];
+    onCloseHandlers:Array<CloseEventCallback> = [];
+
+    onError:(cb:NoParamCallback) => UnsubscribeFunction;
+    onMessage:(cb:MessageEventCallback) => UnsubscribeFunction;
+    onOpen:(cb:NoParamCallback) => UnsubscribeFunction;
+    onClose:(cb:CloseEventCallback) => UnsubscribeFunction;
 
     constructor(hostname:string, port:string|number) {
         this.id = uuid.v4();
         this.ws = new WebSocket(`ws://${hostname}:${port}`);
-        this.ws.onmessage = this.onMessage;
+        this.ws.onmessage = (messageEvent:MessageEvent) => {
+            if (typeof messageEvent.data != 'string') {
+                console.warn( // eslint-disable-line
+                    'Expected string data', messageEvent);
+                return;
+            }
+            const message = JSON.parse(messageEvent.data);
+            if (message.source === this.id) {
+                return;
+            }
+            this.onMessageHandlers.forEach(cb => cb(message));
+            const { id, payload } = message;
+            const response = this.waitingResponse.get(id);
+            if (response) {
+                response.resolve(payload);
+            }
+        };
+        this.ws.onopen = () => this.onOpenHandlers.forEach(cb => cb());
+        this.ws.onclose = (closeEvent:CloseEvent) => this.onCloseHandlers.forEach(
+            cb => cb(closeEvent));
+        this.ws.onerror = () => this.onErrorHandlers.forEach(cb => cb());
+
+        function makeOnEventFn<F>(queue:Array<F>) : (cb:F) => UnsubscribeFunction {
+            return (cb:F) => {
+                queue.push(cb);
+                return () => {
+                    const index = queue.indexOf(cb);
+                    if (index === -1) {
+                        console.warn( // eslint-disable-line
+                            'Could not find handler to remove, are you calling ' +
+                            'unsubscribe function more than once?');
+                        return;
+                    }
+                    queue.splice(index, 1);
+                };
+            };
+        }
+        this.onError = makeOnEventFn(this.onErrorHandlers);
+        this.onClose = makeOnEventFn(this.onCloseHandlers);
+        this.onOpen = makeOnEventFn(this.onOpenHandlers);
+        this.onMessage = makeOnEventFn(this.onMessageHandlers);
     }
 
-    onMessage = (messageEvent:MessageEvent) => {
-        if (typeof messageEvent.data != 'string') {
-            console.warn( // eslint-disable-line
-                'Expected string data', messageEvent);
-            return;
-        }
-        const message = JSON.parse(messageEvent.data);
-        if (message.source === this.id) {
-            return;
-        }
-        const { id, payload } = message;
-        const response = this.waitingResponse.get(id);
-        if (response) {
-            response.resolve(payload);
-        }
-    };
 
     getEntries(
         objectId:RemoteObjectId, iteratorId:?number, config: ?GetEntriesConfig) : Promise<{}> {
